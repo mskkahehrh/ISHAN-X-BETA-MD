a19728d456d97ee04c792435c8de5c80513217e0
xbrovv <ishanxbro847@gmail.com>
2026-02-25 12:27:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function connectToWA() {
       const botJid = ishan.user.id.split(":")[0] + "@s.whatsapp.net";
 
       await ishan.sendMessage(botJid, {
-        image: { url: `https://files.catbox.moe/jt7099.png` },
+        image: { url: `https://files.catbox.moe/evewa0.jpg` },
         caption: up
       });
 
